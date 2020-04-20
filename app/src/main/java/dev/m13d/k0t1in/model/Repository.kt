edf69e5be871efadc7e61037024ea82e43947c30
3yebmb <3yebmb@gmail.com>
2020-04-20 12:09:54 +0500

    change repository to redirect methods

--- app/src/main/java/dev/m13d/k0t1in/model/Repository.kt
+++ app/src/main/java/dev/m13d/k0t1in/model/Repository.kt
@@ -1,33 +1,11 @@
 package dev.m13d.k0t1in.model
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import dev.m13d.k0t1in.data.provider.FireStoreProvider
+import dev.m13d.k0t1in.data.provider.RemoteDataProvider
 
 object Repository {
-
-    private val notesLiveDate = MutableLiveData<List<Note>>()
-    private val notes: MutableList<Note> = mutableListOf()
-
-    init {
-        notesLiveDate.value = notes
-    }
-
-    fun getNotes(): LiveData<List<Note>> {
-        return notesLiveDate
-    }
-
-    fun saveNote(note: Note) {
-        addOrReplace(note)
-        notesLiveDate.value = notes
-    }
-
-    private fun addOrReplace(note: Note) {
-        for(i in 0 until notes.size) {
-            if (notes[i] == note) {
-                notes.set(i, note)
-                return
-            }
-        }
-        notes.add(note)
-    }
+    private val remoteProvider: RemoteDataProvider = FireStoreProvider()
+    fun getNotes() = remoteProvider.subscribeToAllNotes()
+    fun saveNote(note: Note) = remoteProvider.saveNote(note)
+    fun getNoteById(id: String) = remoteProvider.getNoteById(id)
 }
