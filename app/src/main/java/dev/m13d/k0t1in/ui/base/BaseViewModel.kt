2446ba5439ded4f14f3e055bc4408e2016cb5b0c
3yebmb <3yebmb@gmail.com>
2020-04-20 12:28:55 +0500

    add classes for base logic and activities

---
+++ app/src/main/java/dev/m13d/k0t1in/ui/base/BaseViewModel.kt
@@ -0,0 +1,10 @@
+package dev.m13d.k0t1in.ui.base
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+open class BaseViewModel<T, S : BaseViewState<T>> : ViewModel() {
+    open val viewStateLiveData = MutableLiveData<S>()
+    open fun getViewState(): LiveData<S> = viewStateLiveData
+}
