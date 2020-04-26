a6c77f841aa2a8c97b0713222671af73c6d52920
3yebmb <3yebmb@gmail.com>
2020-04-26 17:34:05 +0500

    change inner class NoteViewHolder

--- app/src/main/java/dev/m13d/k0t1in/ui/main/MainAdapter.kt
+++ app/src/main/java/dev/m13d/k0t1in/ui/main/MainAdapter.kt
@@ -3,17 +3,15 @@ package dev.m13d.k0t1in.ui.main
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import dev.m13d.k0t1in.R
-import dev.m13d.k0t1in.model.Colour
+import dev.m13d.k0t1in.extensions.getColorInt
 import dev.m13d.k0t1in.model.Note
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_note.*
+import kotlinx.android.synthetic.main.item_note.view.*
 
-class MainAdapter(private val onItemClickListener: OnItemClickListener) : RecyclerView.Adapter<MainAdapter.NoteViewHolder>() {
-    interface OnItemClickListener {
-        fun onItemClick(note: Note)
-    }
+class MainAdapter(private val onItemClickListener: (Note) -> Unit) : RecyclerView.Adapter<MainAdapter.NoteViewHolder>() {
 
     var notes: List<Note> = listOf()
     set(value) {
@@ -21,36 +19,27 @@ class MainAdapter(private val onItemClickListener: OnItemClickListener) : Recycl
         notifyDataSetChanged()
     }
 
-    override fun getItemCount() = notes.size
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val view = inflater.inflate(R.layout.item_note, parent, false)
+
         return NoteViewHolder(view)
     }
+
+    override fun getItemCount() = notes.size
 
     override fun onBindViewHolder(holder: NoteViewHolder, position: Int): Unit {
         holder.bind(notes[position])
     }
 
-    inner class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val title = itemView.findViewById<TextView>(R.id.title)
-        private val body = itemView.findViewById<TextView>(R.id.body)
+    inner class NoteViewHolder(override val containerView: View) :
+            RecyclerView.ViewHolder(containerView), LayoutContainer {
         fun bind(note: Note) {
-            title.text = note.title
-            body.text = note.note
-            val color = when(note.color) {
-                Colour.WHITE -> R.color.color_white
-                Colour.VIOLET -> R.color.color_violet
-                Colour.YELLOW -> R.color.color_yello
-                Colour.RED -> R.color.color_red
-                Colour.PINK -> R.color.color_pink
-                Colour.GREEN -> R.color.color_green
-                Colour.BLUE -> R.color.color_blue
-            }
+//            title.text = note.title
+//            body.text = note.note
 
-            itemView.setBackgroundColor(ContextCompat.getColor(itemView.context, color))
-            itemView.setOnClickListener {onItemClickListener.onItemClick(note)}
+            itemView.setBackgroundColor(note.color.getColorInt(itemView.context))
+            itemView.setOnClickListener { onItemClickListener(note) }
         }
     }
 }
