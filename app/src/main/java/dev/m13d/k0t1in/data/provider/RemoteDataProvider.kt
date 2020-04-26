2bdd69e2ee0d75dcd6dc1b356894a49fc8f43a42
3yebmb <3yebmb@gmail.com>
2020-04-26 17:48:43 +0500

    add function deleteNote

--- app/src/main/java/dev/m13d/k0t1in/data/provider/RemoteDataProvider.kt
+++ app/src/main/java/dev/m13d/k0t1in/data/provider/RemoteDataProvider.kt
@@ -2,12 +2,13 @@ package dev.m13d.k0t1in.data.provider
 
 import androidx.lifecycle.LiveData
 import dev.m13d.k0t1in.model.Note
-import dev.m13d.k0t1in.model.NoteResult
+import dev.m13d.k0t1in.model.Result
 import dev.m13d.k0t1in.model.User
 
 interface RemoteDataProvider {
-    fun subscribeToAllNotes(): LiveData<NoteResult>
-    fun getNoteById(id: String): LiveData<NoteResult>
-    fun saveNote(note: Note) : LiveData<NoteResult>
+    fun subscribeToAllNotes(): LiveData<Result>
+    fun getNoteById(id: String): LiveData<Result>
+    fun saveNote(note: Note) : LiveData<Result>
     fun getCurrentUser(): LiveData<User?>
+    fun deleteNote(noteId: String): LiveData<Result>
 }
